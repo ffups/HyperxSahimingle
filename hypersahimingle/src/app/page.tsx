cd1279ae121d0ddb345445d385bcf644864edcb6
ffups <142579057+ffups@users.noreply.github.com>
2025-02-20 10:25:30 +0100

    v3 now with qr code

--- hypersahimingle/src/app/page.tsx
+++ hypersahimingle/src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <img src="/v2.png" alt="hyperisland industry mingle map"  />
+      <img src="/v3.png" alt="hyperisland industry mingle map"  />
     </div>
   );
 }
