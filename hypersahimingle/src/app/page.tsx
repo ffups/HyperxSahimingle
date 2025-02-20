bdc20026fd9a19e410bf506b8744b9fbfe8d5648
ffups <142579057+ffups@users.noreply.github.com>
2025-02-20 15:12:37 +0100

    updated map

--- hypersahimingle/src/app/page.tsx
+++ hypersahimingle/src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <img src="/v3.png" alt="hyperisland industry mingle map"  />
+      <img src="/v5.png" alt="hyperisland industry mingle map"  />
     </div>
   );
 }
